a2d2f822a767645e4fb88706f3f1b7745e589108
Leo.Hu <lalalaleo.hu@hotmail.com>
2017-07-03 23:35:28 +0800

    logo加入

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -8,16 +8,20 @@ import { Button, Row, Form, Input } from 'antd'
 function Login(){
     return(
         <div className="login">
+            <div className="logo">
+                <img src="../public/logo.png"/>
+                <span>Enterprise learning platform</span>
+            </div>
             <Input size="large"  placeholder="Username" />
             <Input size="large" type="password"  placeholder="Password" />
             <Row>
                 <Button type="primary" size="large" >
                     Sign in
                 </Button>
-                <p>
+                {/*<p>
                     <span>Username：guest</span>
                     <span>Password：guest</span>
-                </p>
+                </p>*/}
             </Row>
         </div>
     );
