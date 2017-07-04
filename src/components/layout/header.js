3b8d44e756240462cf7faf2d03849e24971c18d1
Leo Hu <lalalaleo.hu@hotmail.com>
2017-07-04 16:34:12 +0800

    header完成

---
+++ src/components/layout/header.js
@@ -0,0 +1,46 @@
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './header.less'
+import {  Row, Col,  Input, Button, Icon } from 'antd'
+const InputGroup = Input.Group;
+
+function Header(){
+    return (
+        <Row type="flex" justify="space-around" align="middle" className="hd">
+            {/*logo、标题*/}
+            <Col span="6">
+                <Row className="logo" type="flex" justify="start" align="middle">
+                    <img className="logo" src="../public/elp_logo.png" />
+                    <span>E L P</span>
+                </Row>
+            </Col>
+            {/*搜索框*/}
+            <Col span="8">
+                <InputGroup className="search">
+                    <Input size="large" placeholder="搜索"></Input>
+                    <Button type="primary" size="large"><Icon type="search" /></Button>
+                </InputGroup>
+            </Col>
+            {/*消息、个人信息*/}
+            <Col span="10">
+                <Row  type="flex" justify="end" align="middle">
+                    <Button  className="message" shape="circle" size="large"><Icon type="message" /></Button>
+                    <Button  className="avatar" shape="circle" size="large">
+                        <img  src="../public/test_avatar.png"></img>
+                    </Button>
+                </Row>
+            </Col>
+        </Row>
+    );
+}
+
+function Tab(){
+    return (
+        <Row className="tab" >
+
+        </Row>
+    );
+}
+
+ReactDOM.render(<Header />,document.getElementById("root"));
