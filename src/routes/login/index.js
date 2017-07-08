198c1d91e9985b3fff169d716274d895614f4bda
Leo Hu <lalalaleo.hu@hotmail.com>
2017-07-08 10:27:41 +0800

    login修改

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 import styles from './index.less';
 import { Button, Row, Form, Input } from 'antd';
 
-
-function Login(){
-    return(
-        <div className="login">
-            <div className="login_logo">
-                <img src="/image/logo.png" alt="logo"/>
-                <span>Enterprise learning platform</span>
+const Login = React.createClass({
+    render: function Login(){
+        return(
+            <div className="login">
+                <div className="login_logo">
+                    <img src="/image/logo.png" alt="logo"/>
+                    <span>Enterprise learning platform</span>
+                </div>
+                <Input size="large"  placeholder="Username" />
+                <Input size="large" type="password"  placeholder="Password" />
+                <Row>
+                    <Button type="primary" size="large" >
+                        Sign in
+                    </Button>
+                </Row>
             </div>
-            <Input size="large"  placeholder="Username" />
-            <Input size="large" type="password"  placeholder="Password" />
-            <Row>
-                <Button type="primary" size="large" >
-                    Sign in
-                </Button>
-            </Row>
-        </div>
-    );
-}
+        );
+    }
+});
 
-// ReactDOM.render(<Login />,document.getElementById('#root'));
 export default { Login };
