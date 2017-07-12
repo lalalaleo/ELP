7d07648118b6ec83a714df7eee245481f9649182
Leo Hu <lalalaleo.hu@hotmail.com>
2017-07-12 16:19:33 +0800

    login 修改页面

--- src/routes/index/index.js
+++ src/routes/index/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './index.less';
 import { Header, IndexToolBar, SegmentCtrl } from '../../components/layout/header.js';
 import { Content } from '../../components/layout/content.js';
-import { Test } from '../test/index'
 import {  Row  } from 'antd'
 
 const IndexPage = React.createClass({
