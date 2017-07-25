6de792003d0f4c744f7f7d0d22b54422b3a8d213
Leo Hu <lalalaleo.hu@hotmail.com>
2017-07-25 14:52:42 +0800

    登录请求发送

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import styles from './index.less';
-import { Content } from '../../components/layout/content.js';
-import { Button, Row, Form, Input } from 'antd';
+import $ from 'jquery'
+import React from 'react'
+import styles from './index.less'
+import { Content } from '../../components/layout/content.js'
+import { Button, Row, Form, Input } from 'antd'
 
 const Login = React.createClass({
     render: function Login(){
         if(sessionStorage.username!=null) window.location.href="/home";
         function btn_Login(){
-            sessionStorage.username="123";
-            window.location.href="/home"
+            var logId=$("input[name='username']").val();
+            var pwd=$("input[name='password']").val();
+            $.ajax({
+                type: "post",
+                url: "http://101.70.100.6:9018/elpcon/login",
+                dataType: "JSON",
+                data: "logId="+logId+"&&pwd="+pwd,
+                success: function(data){
+                    // console.log(data);
+                    if(data.code==200){
+                        document.cookie="SessionId="+data.data.SessionId; 
+                        sessionStorage.User=data.data.User;            
+                        sessionStorage.Office=data.data.Office;
+                        // window.location.href="/home";   
+                    }
+                },
+            });
         }
         return(
             <div id="Login" className="Container">
@@ -18,8 +34,8 @@ const Login = React.createClass({
                             <img src="/image/logo.png" alt="logo"/>
                             <span>Enterprise learning platform</span>
                         </div>
-                        <Input size="large"  placeholder="Username" />
-                        <Input size="large" type="password"  placeholder="Password" />
+                        <Input name="username" size="large"  placeholder="Username" />
+                        <Input name="password" size="large" type="password"  placeholder="Password" />
                         <Row>
                             <Button type="primary" size="large" onClick={btn_Login}>
                                 Sign in
